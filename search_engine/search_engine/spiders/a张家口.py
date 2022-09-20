0e8e385da1653dd5a2f63d6f6b82d4a42e06e644
Ivo <1070642565@qq.com>
2022-09-20 15:33:09 +0800

    add search_engine 张家口

---
+++ search_engine/search_engine/spiders/a张家口.py
@@ -0,0 +1,50 @@
+from typing import Any, Generator, Iterable, List, Optional, Union
+from functools import reduce
+
+from search_engine.basepro import ZhengFuBaseSpider
+from scrapy.responsetypes import Response
+from scrapy import Selector
+
+
+class A张家口Spider(ZhengFuBaseSpider):
+    name: str = '张家口'
+    api: str = 'https://www.zjk.gov.cn/tseach/searchKeywords?keyword={keyword}&fieldSt=1&size=20&startTime=&endTime=&pid=&p={page}&classId=&siteId=1'
+    method: str = 'GET'
+    data: dict[str, Any] = {}
+    debug: bool = False
+
+
+    def edit_page(self, response: Selector) -> int:
+        """
+        input: response
+        return: int
+        """
+        data = response.json()
+        info_class = data['data']['infoClass']
+        total = sum([int(c['pcout']) for c in info_class])
+        return int(total) // 20 + 1
+
+    def edit_items_box(self, response: Selector) -> Union[Any, Iterable[Any]]:
+        """
+        从原始响应解析出包含items的容器
+        input: response
+        return: items_box
+        """
+        data = response.json()
+        return data['data']['infos']['content']
+
+    def edit_item(self, item: Any) -> Optional[dict[str, Union[str, int]]]:
+        """
+        将从items容器中迭代出的item解析出信息
+        input: items
+        return: item_dict
+        """
+        result = {
+            'title': item['content_title'],
+            'url': item['content_pageUrl'],
+            'source': item['content_orgName'],
+            'date': item['content_addTime'],
+            'type': item['content_className'],
+        }
+        return result
+
