75597f371a29b1f54202444474bfd1f00a64a675
Ivo <1070642565@qq.com>
2022-09-20 17:45:53 +0800

    fix article_crawler 衡水

--- article_crawler/article_crawler/spiders/a衡水.py
+++ article_crawler/article_crawler/spiders/a衡水.py
@@ -10,3 +10,11 @@ class A衡水Spider(baseSpider):
             'content': response.css("#barrierfree_container > div.neirong > div > div.nr_P > p  ::text").getall()
         }
         return result
+
+    @baseSpider.parser('衡水', 'www.hengshui.gov.cn')
+    def parser_2(self, response, **kwargs):
+        result = {
+            'content': response.css("div.nr_P  ::text").getall()
+        }
+        return result
+
