2c0e3893afe6e5f9ee886dc150adec10a6030de6
Ivo <1070642565@qq.com>
2022-10-13 11:01:34 +0800

    add article_crawler 日照

--- article_crawler/article_crawler/spiders/a淄博.py
+++ article_crawler/article_crawler/spiders/a淄博.py
@@ -7,5 +7,5 @@ class A淄博Spider(baseSpider):
     @baseSpider.parser('淄博', 'cg.zibo.gov.cn')
     def parser_1(self, response, **kwargs):
         return {
-            'content': response.css("#details-content  :not(script):not(style)::text").get()
+            'content': response.css("#details-content  :not(script):not(style)::text").getall()
         }
