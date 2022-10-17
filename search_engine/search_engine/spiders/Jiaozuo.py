d76a31e547ec8418142cb9cf22cece50899564db
wxy <2639536908@qq.com>
2022-10-17 22:26:02 +0800

    add 焦作

--- search_engine/search_engine/spiders/Jiaozuo.py
+++ search_engine/search_engine/spiders/Jiaozuo.py
@@ -7,18 +7,45 @@ from scrapy import Selector
 
 class JiaozuoSpider(ZhengFuBaseSpider):
     name: str = '焦作'
-    api: str = 'www.jiaozuo.gov.cn'
-    method: str = 'default'
-    data: dict[str, Any] = {}
+    api: str = 'http://www.jiaozuo.gov.cn/search/SolrSearch/searchData'
+    method: str = 'POST'
     debug: bool = False
+    data = {
+        'q': '{keyword}',
+        'catalogId': '',
+        'type': '',
+        'allWord': '',
+        'noWord': '',
+        'timeType': '',
+        'sort': '',
+        'order': '',
+        'forCatalogType': '0',
+        'token4': '4243bdcb86554de0b1ba92050a3365df',
+        'siteId': '',
+        'offset': '{page}',
+        'limit': '8',
+        'infoType': ''
+    }
 
+    # custom_settings: Optional[dict] = {
+    #     'DOWNLOADER_MIDDLEWARES': {
+    #         'search_engine.middlewares.WordTokenDownloaderMiddleware': 543,
+    #     },
+    #     'COOKIES_ENABLED': False,
+    #     'DOWNLOAD_DELAY': 0.5,
+    # }
+    # token_url = 'http://www.jiaozuo.gov.cn/search/SolrSearch/s'
+
+    def edit_data(self, data: dict, keyword: str, page: int, **kwargs) -> dict[str, Any]:
+        data['offset'] = int((page-1) * 8)
+        return data
 
     def edit_page(self, response: Selector) -> int:
         """
         input: response
         return: int
         """
-        raise NotImplementedError()
+        return int(response.json()["totalPage"])
 
     def edit_items_box(self, response: Selector) -> Union[Any, Iterable[Any]]:
         """
@@ -26,7 +53,7 @@ class JiaozuoSpider(ZhengFuBaseSpider):
         input: response
         return: items_box
         """
-        raise NotImplementedError()
+        return response.json()["rows"]
 
     def edit_items(self, items_box: Any) -> Iterable[Any]:
         """
@@ -43,6 +70,11 @@ class JiaozuoSpider(ZhengFuBaseSpider):
         return: item_dict
         """
         result = {
+            "title": item.get("articleTitle", ""),
+            "url": "http://www.jiaozuo.gov.cn" + item.get("articleUri", ""),
+            "date": item.get("articlePublishTime", ""),
+            "source": item.get("siteName", ""),
+            "type": item.get("catalogName", ""),
         }
         return result
 
