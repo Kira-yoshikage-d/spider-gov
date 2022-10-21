ef568ce721d33d05cc4f924a7bb4865654c30154
Ivo <1070642565@qq.com>
2022-10-21 15:05:37 +0800

    fix 株洲

--- search_engine/search_engine/spiders/z株洲.py
+++ search_engine/search_engine/spiders/z株洲.py
@@ -1,20 +1,29 @@
+import scrapy
 from search_engine.basepro import ZhengFuBaseSpider
-from scrapy.responsetypes import Response
-from scrapy import Selector
-class z株洲Spider(ZhengFuBaseSpider):
+
+
+class Z株洲(ZhengFuBaseSpider):
+    """TODO crawl"""
     name = '株洲'
-    method = "GET"
     api = "http://searching.hunan.gov.cn/hunan/973000000/news?q={keyword}&searchfields=&sm=0&columnCN=&iszq=&aggr_iszq=&p={page}&timetype=timeqb"
-    debug = False
+    method = "GET"
+    start_page = 0
 
-    def edit_page(self, response: Response) -> int:
-        pages :str =response.css("div.time-limit.result>p::text").re("相关结果约(.*?)个")[0]
-        return int(pages)//10+1
+    def edit_page(self, response):
+        total_items_num = response.css("div.time-limit.result").re(
+            "相关结果约(.*)个")[0]
+        total_page_num = int(total_items_num) // 10 + 1
+        return total_page_num
 
-    def edit_items_box(self, response: Response) -> Selector:
-        return response.css("li.active")
+    def edit_items_box(self, response):
+        items_box = response.css("div#hits")
+        return items_box
 
-    def edit_item(self, item: Selector) -> dict:
+    def edit_items(self, items_box):
+        items = items_box.css("li")
+        return items
+
+    def edit_item(self, item):
         data = {
             'url': item.css('div.title > a::attr(href)').get(),
             'title': item.css('div.title > a::attr(title)').getall(),
