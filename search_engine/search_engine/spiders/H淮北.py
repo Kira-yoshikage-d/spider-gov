bef741ae58904c7efbd8879ea12b6418a2d4743b
Ivo <1070642565@qq.com>
2022-10-21 14:32:44 +0800

    fix 淮北

--- search_engine/search_engine/spiders/H淮北.py
+++ search_engine/search_engine/spiders/H淮北.py
@@ -1,34 +1,34 @@
+import scrapy
 from search_engine.basepro import ZhengFuBaseSpider
-from scrapy.responsetypes import Response
-from scrapy import Selector
 
-class H淮北Spider(ZhengFuBaseSpider):
+
+class H淮北(ZhengFuBaseSpider):
     name = '淮北'
-    api = "https://www.huaibei.gov.cn/site/tpl/3361?isAllSite=true&platformCode=&siteId=&columnId=&columnIds=&typeCode=&beginDate=&endDate=&fromCode=title&keywords={keyword}&excColumns=&datecode=&sort=intelligent&type=&tableColumnId=&subkeywords=&orderType=0&indexNum=&fileNum=&pid=&language=&flag=false&searchType=&searchTplId=&fuzzySearch=true&internalCall=&catIds=&colloquial=false&pageIndex={page}&pageSize=10"
+    allowed_domains = ['www.huaibei.gov.cn']
+    start_urls = ['http://http://www.huaibei.gov.cn//']
+    api = "https://www.huaibei.gov.cn/site/tpl/3361?isAllSite=true&platformCode=&siteId=&columnId=&columnIds=&typeCode=articleNews,pictureNews,videoNews,policyDoc,explainDoc&beginDate=&endDate=&fromCode=&keywords={keyword}&excColumns=&datecode=&sort=intelligent&type=&tableColumnId=&subkeywords={keyword}&orderType=0&indexNum=&fileNum=&pid=&language=&flag=false&searchType=&searchTplId=&fuzzySearch=true&internalCall=&catIds=&colloquial=true&pageIndex={page}&pageSize=10"
+
     method = "GET"
-    debug = False
 
-    def edit_page(self, response: Response):
-        total1 = response.css("div.searchType-column>ul>li:nth-child(1)>a>span::text").get()
-        total2 = response.css("div.searchType-column>ul>li:nth-child(2)>a>span::text").get()
-        total3 = response.css("div.searchType-column>ul>li:nth-child(3)>a>span::text").get()
-        total4 = response.css("div.searchType-column>ul>li:nth-child(4)>a>span::text").get()
-        total = int(total1)+int(total2)+int(total3)+int(total4)
-        pages = int(total)//10+1
-        print(pages)
-        return pages
+    def edit_page(self, response):
+        total_items = response.css("div.searchType-column > ul:nth-child(1) > li:nth-child(1) > a:nth-child(1) > span:nth-child(1)::text").get()
+        total_page = int(total_items) // 10 + 1
+        return total_page
 
-    def edit_items_box(self, response: Response) -> Selector:
-        items_box = response.css("#search_list > ul")
+    def edit_items_box(self, response):
+        items_box = response.css("div#search_list")
         return items_box
+
+    def edit_items(self, items_box):
+        items = items_box.css("ul.search-list")
+        return items
 
     def edit_item(self, item):
         data = {
             'url':item.css("li.search-title > a::attr(href)").get(),
             'title': item.css("li.search-title > a::attr(title)").get(),
             'type': item.css("li.search-title > span::text").get(),
-            'source':item.css("li.search-resources>span:nth-child(2)::text").get(),
-            'date':item.css("li.search-resources>span:nth-child(3)::text").get()
+            'source':item.css("li.search-resources > span.author::text").get(),
+            'date':item.css("li.search-resources > span.date::text").get()
         }
         return data
-
