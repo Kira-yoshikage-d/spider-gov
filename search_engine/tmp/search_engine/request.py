ebbc51aae61b0ae2630630aad7454143f6ae4b4f
Ivo <1070642565@qq.com>
2022-10-22 07:47:00 +0800

    fix .gitignore spiderkeeper

--- search_engine/tmp/search_engine/request.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-from typing import Optional
-
-from scrapy.http.request.json_request import JsonRequest as OriginJsonRequest
-
-class JsonRequest(OriginJsonRequest):
-    def __init__(self, *args, formdata: dict, dumps_kwargs: Optional[dict] = None, **kwargs) -> None:
-        super().__init__(*args, data=formdata, dumps_kwargs=dumps_kwargs, **kwargs)
