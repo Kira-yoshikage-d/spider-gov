a02d03ebad819dd1685f7c5f5828b493f383b2e9
wxy <2639536908@qq.com>
2023-08-21 19:40:25 +0800

    fix 拉萨、安阳

--- article_crawler/article_crawler/spiders/a柳州.py
+++ article_crawler/article_crawler/spiders/a柳州.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from article_crawler.baseSpider import baseSpider
 
 
@@ -12,20 +11,9 @@ class A柳州Spider(baseSpider):
         }
 
     @baseSpider.parser('柳州', 'swj.liuzhou.gov.cn')
-    def parser_1(self, response, **kwargs):
+    def parser_2(self, response, **kwargs):
         return {
             'content': response.css('div.contentTextBox ::text').getall()
         }
-=======
-from article_crawler.baseSpider import baseSpider
 
 
-class a柳州Spider(baseSpider):
-    name = '柳州'
-
-    @baseSpider.parser('柳州', 'www.liuzhou.gov.cn')
-    def parser_1(self, response, **kwargs):
-        return {
-            'content': response.css('div.contentTextBox  :not(script):not(style)::text').getall(),
-        }
->>>>>>> 20b70eee9a3d24e3594605de61b7683cffe1e7f6
